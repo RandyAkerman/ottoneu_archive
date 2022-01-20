ad8aa9b001b3efddb09a0090039f3bbe1f7b533a
Randy Akerman <rakerman@redhat.com>
2022-01-19 20:35:01 -0500

    added some data clean-up to each function and created an jupyter nb for the start of an analysis

--- ottoneu/data.py
+++ ottoneu/data.py
@@ -6,6 +6,8 @@ def roster_data(league_number):
 
     rosters = pd.read_csv(url)
 
+    rosters.Salary = rosters.Salary.str.replace(',','').str.replace('$','').astype('float')
+
     return(rosters)
 
 def player_values(sheet_id, sheet_name):
@@ -13,6 +15,16 @@ def player_values(sheet_id, sheet_name):
 
     url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 
-    values = pd.read_csv(url)
+    values = pd.read_csv(url, usecols=[0,1,2,3])
+
+    # values.dropna(axis=1, how="all", inplace=True)
+
+    values.rename(columns={"$":"value"}, inplace=True)
+
+    values.dropna(axis=0,how="any", inplace=True)
+
+    values.otto = values.otto.astype("int")
+
+    values.drop(axis=1, columns=["Name", "Team"], inplace=True)
 
     return(values)
